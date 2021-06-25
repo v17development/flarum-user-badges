21b1fa7693aad73c8da8df1739d57b1cdadf3c39
Jasper Vriends <4417659+jaspervriends@users.noreply.github.com>
2021-06-25 23:37:38 +0200

    Update UserBadgeListState.js

--- js/src/forum/states/UserBadgeListState.js
+++ js/src/forum/states/UserBadgeListState.js
@@ -1,4 +1,4 @@
-import PaginatedListState from 'flarum/common/states/PaginatedListState';
+import PaginatedListState from "flarum/common/states/PaginatedListState";
 
 export default class UserBadgeListState extends PaginatedListState {
   constructor() {
@@ -8,6 +8,6 @@ export default class UserBadgeListState extends PaginatedListState {
   }
 
   get type() {
-    return 'user_badges';
+    return "user_badges";
   }
 }

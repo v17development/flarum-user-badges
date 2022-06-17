a81cee8f661a2479666294e4cbbf0c82a7c5eaa2
datlechin <datlechin@gmail.com>
2022-06-17 21:42:08 +0700

    Add `More` badges link

--- js/src/forum/addBadgeListUserCard.js
+++ js/src/forum/addBadgeListUserCard.js
@@ -1,29 +1,44 @@
 import app from 'flarum/forum/app';
 import { extend } from 'flarum/common/extend';
 import UserCard from 'flarum/forum/components/UserCard';
+import Link from 'flarum/common/components/Link';
 import UserBadge from '../common/components/UserBadge';
 import BadgeModal from './components/BadgeModal';
 
 export default function addBadgeListUserCard() {
   extend(UserCard.prototype, 'infoItems', function (items) {
-    const userBadges = this.attrs.user.userBadges();
-    const limitedBadges = userBadges.slice(0, app.forum.attribute('numberOfBadgesOnUserCard'));
+    const user = this.attrs.user;
+    const userBadges = user.userBadges();
 
     if (userBadges.length < 1 || !app.forum.attribute('showBadgesOnUserCard')) return;
 
-    items.add(
-      'badges',
-      limitedBadges.map((userBadge) => (
-        <UserBadge
-          badge={userBadge.badge()}
-          onclick={() =>
-            app.modal.show(BadgeModal, {
-              badge: userBadge.badge(),
-              userBadgeData: userBadge,
-            })
-          }
-        />
-      ))
-    );
+    const limit = app.forum.attribute('numberOfBadgesOnUserCard');
+    const badges = userBadges.slice(0, limit).map((userBadge) => (
+      <UserBadge
+        badge={userBadge.badge()}
+        onclick={() =>
+          app.modal.show(BadgeModal, {
+            badge: userBadge.badge(),
+            userBadgeData: userBadge,
+          })
+        }
+      />
+    ));
+
+    if (badges.length < userBadges.length) {
+      const count = userBadges.length - badges.length;
+      badges.push(
+        <Link
+          href={app.route('user.badges', {
+            username: user.username(),
+          })}
+          className="UserBadge more-badges"
+        >
+          {app.translator.trans('v17development-flarum-badges.forum.badge.others_link', { count })}
+        </Link>
+      );
+    }
+
+    items.add('badges', badges);
   });
 }
